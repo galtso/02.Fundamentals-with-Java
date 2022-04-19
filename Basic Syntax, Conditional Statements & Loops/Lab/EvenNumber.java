00079a00dd01e922d19d16c49d1b11a4166b37ab
galtso <galcho_ta@yahoo.com>
2022-04-19 07:03:21 +0300

    Added Even Number

---
+++ Basic Syntax, Conditional Statements & Loops/Lab/EvenNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = Integer.parseInt(scan.nextLine());
+        while (number % 2 != 0) {
+            System.out.println("Please write an even number.");
+            number= scan.nextInt();
+        }
+        System.out.printf("The number is: %d", Math.abs(number));
+
+    }
+}
