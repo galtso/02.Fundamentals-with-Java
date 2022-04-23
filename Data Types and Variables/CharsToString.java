5be7c44b9e8cd897417357d02853610cbf95c448
galtso <galcho_ta@yahoo.com>
2022-04-23 06:37:22 +0300

    Added CharsToString.java

---
+++ Data Types and Variables/CharsToString.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CharsToString {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char firstChar = scan.nextLine().charAt(0);
+        char secondChar = scan.nextLine().charAt(0);
+        char thirdChar = scan.nextLine().charAt(0);
+        String result = "";
+        System.out.println(result+firstChar+secondChar+thirdChar);
+    }
+}
