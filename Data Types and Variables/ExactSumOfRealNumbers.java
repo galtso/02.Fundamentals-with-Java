1dd496298685ad54038bb81a12321f0a14736214
galtso <galcho_ta@yahoo.com>
2022-04-23 06:13:41 +0300

    Added ExactSumOfRealNumbers.java

---
+++ Data Types and Variables/ExactSumOfRealNumbers.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class ExactSumOfRealNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        BigDecimal result = new BigDecimal("0");
+        for (int i = 0; i < n; i++) {
+            BigDecimal number = new BigDecimal(scan.nextLine());
+            result = result.add(number);
+        }
+        System.out.println(result);
+    }
+}
